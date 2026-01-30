26ac807265911f27a1b83677b44053758d88e804
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-30 18:59:35 -0500

    Holder of package Random

---
+++ test/org/example/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package org.example.fizzbuzz;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    static final Random RANDOM = new Random(System.currentTimeMillis() << 3);
+
+}
