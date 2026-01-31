f3dd8b4127b62632c1353145f7202b999c33352d
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-30 19:06:34 -0500

    Failing process FizzBuzz test

--- test/org/example/fizzbuzz/ClassificationsTest.java
+++ test/org/example/fizzbuzz/ClassificationsTest.java
@@ -1,7 +1,18 @@
 package org.example.fizzbuzz;
 
+import static org.example.fizzbuzz.FizzBuzzTest.RANDOM;
+
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class ClassificationsTest {
+
+    @Test
+    void testFizzBuzzProcess() {
+        int n = RANDOM.nextInt();
+        String expected = "FizzBuzz";
+        Object actual = Classifications.FIZZBUZZ.process(n);
+        assertEquals(expected, actual);
+    }
 
 }
