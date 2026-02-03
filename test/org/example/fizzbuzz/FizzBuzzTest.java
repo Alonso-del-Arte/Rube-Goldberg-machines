57503edc45ae1ac2e6eccb2fc749a68aac15d576
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-03 00:14:46 -0500

    Failing fizzBuzz( ) tests

--- test/org/example/fizzbuzz/FizzBuzzTest.java
+++ test/org/example/fizzbuzz/FizzBuzzTest.java
@@ -3,9 +3,32 @@ package org.example.fizzbuzz;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
     static final Random RANDOM = new Random(System.currentTimeMillis() << 3);
+
+    @Test
+    void testFizzBuzzNegative() {
+        int propNum = RANDOM.nextInt() | Integer.MIN_VALUE;
+        int remainder = propNum % 15;
+        int n = propNum - remainder;
+        String expected = "FizzBuzz";
+        Object actual = FizzBuzz.fizzBuzz(n);
+        String message = "Reckoning FizzBuzz for " + n;
+        assertEquals(expected, actual, message);
+    }
+
+    @Test
+    void testFizzBuzzPositive() {
+        int propNum = RANDOM.nextInt() & Integer.MAX_VALUE;
+        int remainder = propNum % 15;
+        int n = propNum - remainder;
+        String expected = "FizzBuzz";
+        Object actual = FizzBuzz.fizzBuzz(n);
+        String message = "Reckoning FizzBuzz for " + n;
+        assertEquals(expected, actual, message);
+    }
 
 }
