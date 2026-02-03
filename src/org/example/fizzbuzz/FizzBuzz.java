d0ae717637d17f40ae654438c059387208b05b96
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-03 00:05:18 -0500

    fizzBuzz( ) stub

--- src/org/example/fizzbuzz/FizzBuzz.java
+++ src/org/example/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,10 @@ package org.example.fizzbuzz;
 
 public class FizzBuzz {
 
+    public static Object fizzBuzz(int n) {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
     public static void main(String[] args) {
         System.out.println("NOT IMPLEMENTED YET");
     }
