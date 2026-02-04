0fb0495d6d9c6326acc47f884ef50f33dafc4b6f
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-04 00:53:13 -0500

    Refactored to be complicated

--- src/org/example/fizzbuzz/FizzBuzz.java
+++ src/org/example/fizzbuzz/FizzBuzz.java
@@ -2,13 +2,24 @@ package org.example.fizzbuzz;
 
 public class FizzBuzz {
 
+    public static final Classifications[] CLASSIFS = Classifications.values();
+
     public static Object fizzBuzz(int n) {
-        return switch (n % 15) {
-            case -12, -9, -6, -3, 3, 6, 9, 12 -> "Fizz";
-            case -10, -5, 5, 10 -> "Buzz";
-            case 0 -> "FizzBuzz";
-            default -> n;
-        };
+        int remainder = n % 15;
+        int square = (remainder * remainder) % 15;
+        try {
+            return CLASSIFS[square].process(n);
+        } catch (ArrayIndexOutOfBoundsException aioobeOuter) {
+            try {
+                return CLASSIFS[square - 3].process(n);
+            } catch (ArrayIndexOutOfBoundsException aioobeMiddle) {
+                try {
+                    return CLASSIFS[square - 8].process(n);
+                } catch (ArrayIndexOutOfBoundsException aioobeInner) {
+                    return CLASSIFS[square - 9].process(n);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
