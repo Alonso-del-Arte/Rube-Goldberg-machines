44f80d213a2d20f334840d2f1e8b405d775a8773
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-04 23:47:36 -0500

    Failing isPowerOfTwo( ) test

--- test/org/oeis/CalculatorTest.java
+++ test/org/oeis/CalculatorTest.java
@@ -1,7 +1,18 @@
 package org.oeis;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
+
+    @Test
+    void testIsPowerOfTwo() {
+        int power = 1;
+        while (power > 0) {
+            String msg = power + " should be a power of 2";
+            assert Calculator.isPowerOfTwo(power) : msg;
+            power <<= 1;
+        }
+    }
 
 }
