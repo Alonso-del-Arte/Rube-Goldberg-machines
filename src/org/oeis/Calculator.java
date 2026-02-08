f8db6cc952f55cf5907935348e49235bb33e0ac1
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-07 20:13:31 -0500

    fibonacci( ) stub

--- src/org/oeis/Calculator.java
+++ src/org/oeis/Calculator.java
@@ -1,7 +1,14 @@
 package org.oeis;
 
+import java.math.BigInteger;
+
 public class Calculator {
 
+    // TODO: Write tests for this
+    public static BigInteger fibonacci(int index) {
+        return BigInteger.TEN;
+    }
+    
     // TODO: Write tests for this
     public static boolean isPowerOfTwo(int n) {
         return n > 0;
